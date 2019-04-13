5857c957019f16133adc44e8fbc1d67cb880ad49
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-13 22:58:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@ def take_a_number(katz_deli, joiner)
 end
 
 def now_serving(katz_deli)
-  if 
-  puts katz_deli[0]
+  if katz_deli.length == 0 
+    puts "There is nobody waiting to be served"
   
 end

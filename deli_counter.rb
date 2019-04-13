82882b6e1461c1644ef4e34f1b37b57187abfef5
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-13 22:56:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ def take_a_number(katz_deli, joiner)
   puts "Welcome, #{joiner}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving
+def now_serving(katz_deli)
   puts katz_deli(0)
   
 end

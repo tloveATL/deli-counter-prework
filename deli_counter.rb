aa86c9165da341ea0f20de24adf56c74b83a901d
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-13 22:42:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,8 @@ def line(katz_deli)
    puts "The line is currently empty."
   else
     current_line = "The line is currently:"
-  katz_deli.each_with_index(1) do |customer, index|
-    current_line << "#{index}. #{customer}"
+  katz_deli.each_with_index do |customer, index|
+    current_line << "#{index + 1}. #{customer}"
   end
   puts current_line
   end

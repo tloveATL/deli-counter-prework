177bb4a04301e65344c0e3183bf6ae03bbc2b4a1
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-13 22:26:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 katz_deli = []
 
 def line(katz_deli)
-    katz_deli.count > 0
+  if katz_deli.count > 0
    puts "The line is currently: #{index + 1}. #{customer}"
   #else 
    # puts "The line is currently empty."

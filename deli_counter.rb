762d5eaa1dea64d5d56af35f36d5a91093204d1e
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-13 22:18:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,8 @@
 katz_deli = []
 
 def line(katz_deli)
-    katz_deli.each_with_index.map do |customer, index|
+  katz_deli.each_with_index.map do |customer, index|
+      
   if katz_deli.count > 0
       puts "The line is currently: #{index + 1}. #{customer}"
   else 

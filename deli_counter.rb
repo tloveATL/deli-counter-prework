52fd515c6c7133a6dae030fceab981a32710523d
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-13 22:20:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   #katz_deli.each_with_index.map do |customer, index|
       
   #if katz_deli.count > 0
-   #   puts "The line is currently: #{index + 1}. #{customer}"
+   puts "The line is currently: #{index + 1}. #{customer}"
   #else 
    # puts "The line is currently empty."
   #end
